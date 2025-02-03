81df39aa4ad9ff31d699568470bf25340c7bdc51
bmacmill <bnobile@gmail.com>
2025-02-03 13:08:05 -0500

    generate 10 die for game
    generate playing die, set in state, render 10 die on game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
+import React from "react"
+
 import './App.css'
 import Die from "./Die.jsx"
 
 function App() {
   
-  let dice = new Array(10).fill("1")
+  // initial dice state
+  const [dice, setDice] = React.useState(generatePlayingDice())
+
   
-  console.log("dice", dice)
-  const gameDice = dice.map((el, i) => {
-    return <Die key={i} number={el} />
-  })
+  // generate full array of 10 dice
+ function generatePlayingDice(){
+    return new Array(10)
+          .fill(0)
+          .map(()=> Math.ceil(Math.random() * 6))
+ }
+  
+// generate 10 die elements on the page
+ const gameDice = dice.map((num, i) => {
+  return <Die key={i} number={num} />
+})
+
+
+
   return (
     
    
       <main>
         <div className="Die-container">
-      {gameDice}
-      </div>
+          {gameDice}
+        </div>
       </main>
 
   )
